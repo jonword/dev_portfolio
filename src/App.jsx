3a3b5fb29a4524f0106e270d14ab7188fa5b5bef
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-01-30 15:21:20 -0600

    add details to project card and update styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App flex justify-between gap-12">
       <Nav />
-      <div className="main-container flex flex-col justify-center mt-28 mr-80">
+      <div className="main-container flex flex-col justify-center mt-28 mr-80 w-full border border-shadow">
         <About />
         <Skills />
         <Projects />
