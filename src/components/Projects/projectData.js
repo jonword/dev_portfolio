dd73f475393a394f8a11e4d30fa29fbaf980df5b
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-02-03 13:08:18 -0600

    project data in searate component

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -1,11 +1,12 @@
-import ecommerce from "../assets/images/projects/ecommerce.jpg";
-import reddit from "../assets/images/projects/reddit-lite.jpg";
-import portfolio from "../assets/images/projects/portfolio.png";
+import ecommerce from "../../assets/images/projects/ecommerce.jpg";
+import reddit from "../../assets/images/projects/reddit-lite.jpg";
 
 const projects = [
   {
+    id: 1,
     title: "Reddit-Lite",
     image: reddit,
+    alt: "Reddit-Lite project",
     techList: "React, Redux, CSS",
     description:
       "A light version of the Reddit app with a functional search bar.",
@@ -14,8 +15,10 @@ const projects = [
   },
 
   {
+    id: 2,
     title: "E-Commerce Product Page",
     image: ecommerce,
+    alt: "E-Commerce project",
     techList: "React, TypeScript, Redux, CSS",
     description:
       "A responsive web-page that allows you to add the item to your cart and updates the cart accordingly.",
